cdb1cf75e3df65dff4312dc977c9be0873d01d6e
Jiří Kašpar <99127217+Jira-K@users.noreply.github.com>
2022-02-19 15:52:26 +0100

    Add files via upload

---
+++ adresar.ps1
@@ -0,0 +1,3 @@
+﻿$dirA=Get-ChildItem -Recurse -Filter 'ISZR_A*.bak'
+$dirB=Get-ChildItem -Recurse -Filter 'ISZR_B*.bak'
+$dirA+$dirB | Select-Object -Property Name,Length,Directory | Export-Csv 'dirF.csv' -NoTypeInformation
